dcf9e55860ca9a27f704cffded0d51267aa4fb8e
Devansh Chawla <devanshchawla@Devanshs-MacBook-Air.local>
2025-04-20 18:38:34 +0530

    Tmdb api data fetch for now playing movies

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,14 +6,13 @@ import {
 } from "../utils/validate";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
+import { USER_AVATAR } from "../utils/constants";
 
 const Login = () => {
   const [isSignIn, setIsSignIn] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
-  const navigate = useNavigate()
   const dispatch = useDispatch()
 
   const name = useRef(null);
@@ -39,11 +38,10 @@ const Login = () => {
         .then((userCredential) => {
           const user = userCredential.user;
           updateProfile(user, {
-            displayName: name.current.value, photoURL: "https://avatars.githubusercontent.com/u/88234517?v=4"
+            displayName: name.current.value, photoURL: USER_AVATAR
           }).then(() => {
             const {uid,email,displayName,photoURL} = auth.currentUser;
             dispatch(addUser({uid,email,displayName,photoURL}))
-            navigate('/browse')
           }).catch((error) => {
             console.log(error)
             setErrorMessage(error.message)
@@ -61,7 +59,6 @@ const Login = () => {
           // Signed in 
           const user = userCredential.user;
           console.log(user)
-          navigate('/browse')
         })
         .catch((error) => {
           const errorCode = error.code;
---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+export const LOGO = 'https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
+
+export const USER_AVATAR = 'https://occ-0-1492-3662.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABeuqjuQsRgqEDlibtJTI5BMf8IxhLlLOeIT6xI4TL57mqv7XHja43gx02S8pZVe8JNGRQXjnrUk1VcsTXqi83tFKPI6OR3k.png?r=bd7'
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYWY2NGY1ZTFiNTUzNjYwOGIxNjNlMjY2MmFmZmU3YSIsIm5iZiI6MTc0NTA2NDg1Ni44NTQsInN1YiI6IjY4MDM5Mzk4ZTQ3NTM0MjVlZmFkNzFmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9JCLpY90X-zI4L71owBx7_kYBOfxXrM9YzFGL6Mz_kw'
+    }
+};
